a046cdbddff7c0a7ea47f462f213ff899cce970a
Miguel-ectil <miguel.ectil@ppc.com.br>
2026-03-31 14:08:25 -0300

    :sparkles: feat: [ADD] Adicionando configurção de acesso ao supabase chamando os dados do .env, Adicionando Models de usuário com as colunas necessarias, Adicionando service de cadastro de usuário, Adicioando rotas de cadastro de usuário, Adicionando arquivo de confirguração do JWT, para proteção de rotas e autenticação, Adicionando arquivo que transforma senha em rash mantendo a seguraça das senhas

---
+++ backend/app/utils/password.py
@@ -0,0 +1,11 @@
+import bcrypt
+
+def hash_password(password: str) -> str:
+    # Gera o hash da senha
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
+
+def check_password(password: str, hashed: str) -> bool:
+    # Compara senha em texto com hash armazenado
+    return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
