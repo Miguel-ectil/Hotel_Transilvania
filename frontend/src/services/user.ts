b40d7c7c97d97164c74a4f5d4805a17f421c86b3
Miguel-ectil <miguel.ectil@ppc.com.br>
2026-04-14 17:23:20 -0300

    :sparkles: feat: [ADD] Adicionando middleware para proteger as rotas do front-end, Adicionando tela de dashboard sem informações, adicionando conteudo da tela de login, adicionando authcontext para tratar token e proteger aplicação, Adicionando service do login

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -10,8 +10,15 @@ export const ServiceUser = () => {
     const resp: AxiosResponse = await httpClient.post(url, form)
     return resp;
   }
+
+  const LoginUser = async (data: { email: string; password: string }): Promise<AxiosResponse> => {
+    const url = `${Url}/login`;
+    const resp: AxiosResponse = await httpClient.post(url, data)
+    return resp;
+  }
     
   return {
-    RegisterUser
+    RegisterUser,
+    LoginUser
   }
 } 
