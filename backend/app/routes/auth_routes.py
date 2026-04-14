1e48e7c2a455abaad1708e45ada0749b4b7f7c8f
Miguel-ectil <miguel.ectil@ppc.com.br>
2026-04-14 16:06:24 -0300

    :sparkles: feat: [ADD] Adicionando assinatura do token a ser gerado ao acessar a aplicação, Adicionando rota do login e do teste de autenticação

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, request, jsonify
 from app.services.auth_service import register_user
 from app.utils.jwt import require_auth
+from app.services.auth_service import login_user
 
 auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.route("/register", methods=["POST"])
-# @require_auth
 def register():
     data = request.get_json()
 
@@ -24,3 +24,27 @@ def register():
         return jsonify(result), 400
 
     return jsonify({"message": "Usuário criado com sucesso"}), 201
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    email = data.get("email").strip().lower()
+    password = data.get("password").strip()
+
+    if not email or not password:
+        return jsonify({"error": "Email e senha são obrigatórios"}), 400
+
+    result = login_user(email, password)
+
+    if "error" in result:
+        return jsonify(result), 401
+
+    return jsonify(result), 200
+
+
+@auth_bp.route("/me", methods=["GET"])
+@require_auth
+def me():
+    return jsonify(request.user)
